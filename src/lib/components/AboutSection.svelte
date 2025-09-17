6dbe74ca460abf63f6041d2fea07e1a1f8a8f9dd
Alireza SadraiiRad <sadraiiali@gmail.com>
2025-09-17 15:19:15 +0200

    feat: Update AboutSection with local image and add new video file

--- src/lib/components/AboutSection.svelte
+++ src/lib/components/AboutSection.svelte
@@ -22,7 +22,7 @@
 			</div>
 			<div>
 				<img 
-					src="https://shamimminoo.com/wp-content/uploads/2025/07/IMG_3632-scaled.jpeg" 
+					src="/img/IMG_3632-scaled.jpeg" 
 					alt="Shamim Minoo performing with harp" 
 					class="w-full h-auto object-cover shadow-deep rounded-lg"
 				>
